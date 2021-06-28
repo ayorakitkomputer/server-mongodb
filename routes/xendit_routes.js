90d7e1e8bffd1bf511e7db732c3dacbb34f483ac
Jethro Pierce <jethro.kapantow@gmail.com>
2021-06-28 20:05:29 +0700

    Update Xendit

--- routes/xendit_routes.js
+++ routes/xendit_routes.js
@@ -4,16 +4,6 @@ const Xendit = require("../controllers/xendit_controller");
 const authentication = require("../middlewares/authentication");
 
 router.use(authentication);
-router.post("/callback", Xendit.allCallback);
-router.get("/va/list", Xendit.getVirtualAccounts);
-router.post("/va/invoice", Xendit.createVirtualAccount);
-router.post("/va/payment", Xendit.payVirtualAccount);
-router.post("/ew/checkout", Xendit.createEWalletPayment);
-router.post("/ew/status", Xendit.getEWalletDetail);
-router.post("/card/checkout", Xendit.createCreditCharge);
-router.post("/card/status", Xendit.getCreditDetail);
-router.post("/retail/checkout", Xendit.createRetailCharge);
-router.post("/retail/status", Xendit.getRetailDetails);
-router.post("/retail/payment", Xendit.payRetailStore);
+router.post("/invoice", Xendit.createInvoice);
 
 module.exports = router;

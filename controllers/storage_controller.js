fbad09105c43a5d6d15c452123985d750b4eca26
Jethro Pierce <jethro.kapantow@gmail.com>
2021-06-30 22:54:06 +0700

    Storage Path Fixed

--- controllers/storage_controller.js
+++ controllers/storage_controller.js
@@ -1,105 +1,102 @@
 const storageValidator = require("../helpers/storage_validator");
-const Storage = require("../models/Storage");
+const Storage = require("../models/storage");
 
 class Controller {
-  static async getStorage(req, res) {
-    let page = parseInt(req.query.page);
-    let limit = 10;
-    const documentsCount = await Storage.findDocumentsCount()
-    const howManyPages = Math.ceil(documentsCount / limit)
+	static async getStorage(req, res) {
+		let page = parseInt(req.query.page);
+		let limit = 10;
+		const documentsCount = await Storage.findDocumentsCount();
+		const howManyPages = Math.ceil(documentsCount / limit);
 
-    if (page <= 0) {
-      res
-        .status(404)
-        .json({ message: "invalid page number, should start with 1" });
-    } else {
-      let skippedData = (page - 1) * limit;
-      Storage.findAll(skippedData, limit)
-        .then((data) => {
-          res.status(200).json({ data, howManyPages });
-        });
-    }
-  }
+		if (page <= 0) {
+			res.status(404).json({ message: "invalid page number, should start with 1" });
+		} else {
+			let skippedData = (page - 1) * limit;
+			Storage.findAll(skippedData, limit).then((data) => {
+				res.status(200).json({ data, howManyPages });
+			});
+		}
+	}
 
-  static getOneStorage(req, res) {
-    const id = req.params.id;
+	static getOneStorage(req, res) {
+		const id = req.params.id;
 
-    Storage.findOne(id).then((data) => {
-      if (data === null) {
-        res.status(404).json({ message: `Data not found` });
-      } else {
-        res.status(200).json(data);
-      }
-    });
-  }
+		Storage.findOne(id).then((data) => {
+			if (data === null) {
+				res.status(404).json({ message: `Data not found` });
+			} else {
+				res.status(200).json(data);
+			}
+		});
+	}
 
-  static postStorage(req, res) {
-    const newStorage = {
-      image: req.body.image,
-      name: req.body.name,
-      capacity: req.body.capacity,
-      type: req.body.type,
-      price: +req.body.price,
-      stock: +req.body.stock,
-    };
+	static postStorage(req, res) {
+		const newStorage = {
+			image: req.body.image,
+			name: req.body.name,
+			capacity: req.body.capacity,
+			type: req.body.type,
+			price: +req.body.price,
+			stock: +req.body.stock,
+		};
 
-    const { errors, errorFlag } = storageValidator(newStorage);
+		const { errors, errorFlag } = storageValidator(newStorage);
 
-    if (errorFlag) {
-      res.status(400).json({ message: errors });
-    } else {
-      Storage.create(newStorage)
-        .then((data) => {
-          res.status(201).json(data.ops[0]);
-        })
-        .catch((err) => {
-          res.status(500).json({ message: "Server Error" });
-        });
-    }
-  }
+		if (errorFlag) {
+			res.status(400).json({ message: errors });
+		} else {
+			Storage.create(newStorage)
+				.then((data) => {
+					res.status(201).json(data.ops[0]);
+				})
+				.catch((err) => {
+					res.status(500).json({ message: "Server Error" });
+				});
+		}
+	}
 
-  static putStorage(req, res) {
-    const id = req.params.id;
+	static putStorage(req, res) {
+		const id = req.params.id;
 
-    const updatedStorage = {
-      image: req.body.image,
-      name: req.body.name,
-      capacity: req.body.capacity,
-      type: req.body.type,
-      price: +req.body.price,
-      stock: +req.body.stock,
-    };
+		const updatedStorage = {
+			image: req.body.image,
+			name: req.body.name,
+			capacity: req.body.capacity,
+			type: req.body.type,
+			price: +req.body.price,
+			stock: +req.body.stock,
+		};
 
-    const { errors, errorFlag } = storageValidator(updatedStorage);
+		const { errors, errorFlag } = storageValidator(updatedStorage);
 
-    if (errorFlag) {
-      res.status(400).json({ message: errors });
-    } else {
-      Storage.update(id, updatedStorage).then((data) => {
-        if (data.matchedCount == 0) {
-          res.status(404).json({ message: `Data not Found` });
-        } else if (data.matchedCount == 1 && data.modifiedCount == 1) {
-          res.status(200).json({ message: `Succesfully edited the Storage` });
-        }
-      });
-    }
-  }
+		if (errorFlag) {
+			res.status(400).json({ message: errors });
+		} else {
+			Storage.update(id, updatedStorage).then((data) => {
+				if (data.matchedCount == 0) {
+					res.status(404).json({ message: `Data not Found` });
+				} else if (data.matchedCount == 1 && data.modifiedCount == 1) {
+					res.status(200).json({ message: `Succesfully edited the Storage` });
+				}
+			});
+		}
+	}
 
-  static deleteStorage(req, res) {
-    const id = req.params.id;
+	static deleteStorage(req, res) {
+		const id = req.params.id;
 
-    Storage.destroy(id)
-      .then((data) => {
-        if (data.deletedCount == 0) {
-          res.status(404).json({ message: `Data not Found` });
-        } else if (data.deletedCount == 1) {
-          res.status(200).json({ message: `Succesfully deleted the Storage` });
-        }
-      })
-      .catch((err) => {
-        res.status(500).json({ message: err.message });
-      });
-  }
+		Storage.destroy(id)
+			.then((data) => {
+				if (data.deletedCount == 0) {
+					res.status(404).json({ message: `Data not Found` });
+				} else if (data.deletedCount == 1) {
+					res.status(200).json({ message: `Succesfully deleted the Storage` });
+				}
+			})
+			.catch((err) => {
+				res.status(500).json({ message: err.message });
+			});
+	}
 }
 
 module.exports = Controller;
